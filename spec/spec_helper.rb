0b1bcf82b6d44182b5af278816273d63e3d05de4
Alexei Mikhailov <a.mikhailov@fun-box.ru>
2013-12-24 18:18:06 +0600

    Rails 4.1 support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@ ActiveRecord::Base.configurations = YAML::load(ERB.new(IO.read(File.join(test_di
 ActiveRecord::Base.establish_connection(ENV['DB'])
 ActiveRecord::Base.logger = Logger.new(ENV['DEBUG'] ? $stderr : '/dev/null')
 ActiveRecord::Migration.verbose = false
+I18n.enforce_available_locales = false if I18n.respond_to?(:enforce_available_locales=)
 load(File.join(test_dir, 'db', 'schema.rb'))
 
 require 'shoulda-matchers'
