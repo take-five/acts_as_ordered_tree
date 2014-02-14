8ed06db6b0c991f99210747a600606c1c48a381a
Alexei Mikhailov <amikhailov83@gmail.com>
2014-02-14 10:37:23 +0000

    AAOT::Node::Movements refactored

--- lib/acts_as_ordered_tree/node/movements.rb
+++ lib/acts_as_ordered_tree/node/movements.rb
@@ -19,59 +19,55 @@ module ActsAsOrderedTree
   # * move_to_child_with_index
   # * move_to_child_with_position
   module Node::Movements
-    # @api private
-    def self.persevering(method)
-      module_eval <<-RUBY, __FILE__, __LINE__ + 1
-        def #{method}_with_persevering(*args)
-          persevering do
-            #{method}_without_persevering(*args)
-          end
-        end
-      RUBY
-
-      alias_method_chain method, :persevering
-    end
-
     # Transform node into root node
     def move_to_root
-      move_to nil, record.root? ? position : nil
+      movement do |to|
+        to.position = record.root? ? position : nil
+      end
     end
-    persevering :move_to_root
 
     # Swap node with higher sibling
     def move_higher
-      move_to parent_id, position - 1
+      movement do |to|
+        to.parent_id = parent_id
+        to.position = position - 1
+      end
     end
-    persevering :move_higher
     alias_method :move_left, :move_higher
 
     # Swap node with lower sibling
     def move_lower
-      move_to parent_id, position + 1
+      movement do |to|
+        to.parent_id = parent_id
+        to.position = position + 1
+      end
     end
-    persevering :move_lower
     alias_method :move_right, :move_lower
 
     # Move node to above(left) of another node
     #
     # @param [ActiveRecord::Base, #to_i] node may be another record of ID
     def move_to_above_of(node)
-      with_target(node) do |target, lower|
-        move_to target.parent_id, lower ? target.position - 1 : target.position
+      movement(node) do |to, target|
+        lower = target.parent_id == parent_id && target.position > position
+
+        to.parent_id = target.parent_id
+        to.position = lower ? target.position - 1 : target.position
       end
     end
-    persevering :move_to_above_of
     alias_method :move_to_left_of, :move_to_above_of
 
     # Move node to bottom (right) of another node
     #
     # @param [ActiveRecord::Base, #to_i] node may be another record of ID
     def move_to_bottom_of(node)
-      with_target(node) do |target, lower|
-        move_to target.parent_id, lower ? target.position : target.position + 1
+      movement(node) do |to, target|
+        lower = target.parent_id == parent_id && target.position > position
+
+        to.parent_id = target.parent_id
+        to.position = lower ? target.position : target.position + 1
       end
     end
-    persevering :move_to_bottom_of
     alias_method :move_to_right_of, :move_to_bottom_of
 
     # Move node to child of another node
@@ -79,10 +75,12 @@ module ActsAsOrderedTree
     # @param [ActiveRecord::Base, #to_i] node may be another record of ID
     def move_to_child_of(node)
       if node
-        persevering do
-          with_target(node) do |target|
-            move_to target.id, nil if target.id != parent_id
-          end
+        movement(node) do |to, target|
+          # don't do anything if new parent is the same
+          return true if target.id == parent_id
+
+          to.parent_id = target.id
+          to.position = nil
         end
       else
         move_to_root
@@ -99,11 +97,9 @@ module ActsAsOrderedTree
       if index >= 0
         move_to_child_with_position node, index + 1
       elsif node
-        persevering do
-          # @todo here 1 redundant query
-          with_target(node) do |target|
-            move_to_child_with_position node, target.children.size + index + 1
-          end
+        movement(node) do |to, target|
+          to.parent_id = target.id
+          to.position = target.children.size + index + 1
         end
       else
         move_to_child_with_position nil, scope.roots.size + index + 1
@@ -115,50 +111,65 @@ module ActsAsOrderedTree
     # @param [ActiveRecord::Base, Fixnum] node
     # @param [Integer, nil] position
     def move_to_child_with_position(node, position)
-      if node
-        with_target(node) do |target|
-          move_to target.id, position
-        end
-      else
-        move_to nil, position
+      movement(node) do |to, target|
+        to.parent_id = target.try(:id)
+        to.position = position
       end
     end
-    persevering :move_to_child_with_position
 
     private
-    def move_to(new_parent_id, new_position)
-      from = Position.new(self, parent_id, position)
-      to = Position.new(self, new_parent_id, new_position)
-
-      # @todo maybe move it to transaction callbacks?
-      return false unless to.valid?
-
-      transition = Position::Transition.new(from, to)
-
-      transaction = Transaction::Factory.create_from_transition(self, transition)
-      transaction.start { true }
+    def movement(target = nil, &block)
+      Movement.start(self, target, &block)
     end
 
-    # @param [ActiveRecord::Base] node
-    # @yield target, lower
-    # @yieldparam [ActsAsOrderedTree::Node] target
-    # @yieldparam [true, false] lower
-    def with_target(node)
-      # reload and lock target node
-      target = scope.lock.find(node).ordered_tree_node
-      # @todo yuck!
-      to_lower = target.parent_id == parent_id && target.position > position
-
-      yield target, to_lower if target.record != record
-    end
-
-    # Wraps block with persevering transaction
-    def persevering
-      PerseveringTransaction.new(record.class.connection).start do
-        reload
-
-        yield
+    class Movement
+      def self.start(node, target, &block)
+        new(node, target, &block).start
       end
-    end
-  end
-end
+
+      # @param [ActsAsOrderedTree::Node] node moved node
+      # @param [ActiveRecord::Base, #to_i]
+      def initialize(node, target, &block)
+        @node = node
+        @target = target
+
+        @from = Position.new(node, node.parent_id, node.position)
+        @to = Position.new(node, nil, nil)
+        @transition = Position::Transition.new(@from, @to)
+
+        @block = block
+      end
+
+      def start
+        persevering do
+          @block.call(@to, target!) if @block
+
+          return false unless valid?
+
+          transaction.start { true }
+        end
+      end
+
+      private
+      def valid?
+        @node.record != @target.try(:record) && @to.valid?
+      end
+
+      def transaction
+        @transaction ||= Transaction::Factory.create_from_transition(@node, @transition)
+      end
+
+      def persevering
+        PerseveringTransaction.new(@node.record.class.connection).start do
+          @node.reload
+
+          yield
+        end
+      end
+
+      def target!
+        @target &&= @node.scope.lock.find(@target).ordered_tree_node
+      end
+    end # class Movement
+  end # module Node::Movements
+end # module ActsAsOrderedTree
