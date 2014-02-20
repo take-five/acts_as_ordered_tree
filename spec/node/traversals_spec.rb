e5fb615c3067baecae04a28132d00c3c5b5f7708
Alexei Mikhailov <amikhailov83@gmail.com>
2014-02-20 16:07:03 +0000

    Move AAOT::Node::Traversal specs to separate file

--- spec/node/traversals_spec.rb
+++ spec/node/traversals_spec.rb
@@ -2,16 +2,19 @@
 
 require 'spec_helper'
 
-describe ActsAsOrderedTree::Node::Traversals do
+describe ActsAsOrderedTree::Node::Traversals, :transactional do
   shared_examples 'ActsAsOrderedTree::Node traversals' do |model, attrs = {}|
-    describe '#root' do
-      let!(:root_1) { create model, attrs }
-      let(:child_1) { create model, attrs.merge(:parent => root_1) }
-      let(:grandchild_1) { create model, attrs.merge(:parent => child_1) }
-      let!(:root_2) { create model, attrs }
-      let(:child_2) { create model, attrs.merge(:parent => root_2) }
-      let(:grandchild_2) { create model, attrs.merge(:parent => child_2) }
+    let(:root_1) { create model, attrs }
+    let(:child_1) { create model, attrs.merge(:parent => root_1) }
+    let(:grandchild_1) { create model, attrs.merge(:parent => child_1) }
+    let(:root_2) { create model, attrs }
+    let(:child_2) { create model, attrs.merge(:parent => root_2) }
+    let(:grandchild_2) { create model, attrs.merge(:parent => child_2) }
 
+    before { [root_1, child_1, grandchild_1].each(&:reload) }
+    before { [root_2, child_2, grandchild_2].each(&:reload) }
+
+    describe '#root' do
       it { expect(root_1.root).to eq root_1 }
       it { expect{root_1.root}.not_to query_database }
       it { expect(child_1.root).to eq root_1 }
@@ -21,6 +24,32 @@ describe ActsAsOrderedTree::Node::Traversals do
       it { expect{root_2.root}.not_to query_database }
       it { expect(child_2.root).to eq root_2 }
       it { expect(grandchild_2.root).to eq root_2 }
+    end
+
+    describe '#self_and_ancestors' do
+      it { expect(root_1.self_and_ancestors).to eq [root_1] }
+      it { expect{root_1.self_and_ancestors}.not_to query_database }
+      it { expect(child_1.self_and_ancestors).to eq [root_1, child_1] }
+      it { expect(grandchild_1.self_and_ancestors).to eq [root_1, child_1, grandchild_1] }
+    end
+
+    describe '#ancestors' do
+      it { expect(root_1.ancestors).to eq [] }
+      it { expect{root_1.ancestors}.not_to query_database }
+      it { expect(child_1.ancestors).to eq [root_1] }
+      it { expect(grandchild_1.ancestors).to eq [root_1, child_1] }
+    end
+
+    describe '#self_and_descendants' do
+      it { expect(root_1.self_and_descendants).to eq [root_1, child_1, grandchild_1] }
+      it { expect(child_1.self_and_descendants).to eq [child_1, grandchild_1] }
+      it { expect(grandchild_1.self_and_descendants).to eq [grandchild_1] }
+    end
+
+    describe '#descendants' do
+      it { expect(root_1.descendants).to eq [child_1, grandchild_1] }
+      it { expect(child_1.descendants).to eq [grandchild_1] }
+      it { expect(grandchild_1.descendants).to eq [] }
     end
   end
 
